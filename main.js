ba45fd6b3850440cf6e46a521aba8220869e8a7e
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-10-24 01:38:22 -0400

    Initial

---
+++ main.js
@@ -0,0 +1,43 @@
+//alert('Hello, world!');
+//alert('Hello, world!');
+//console.log('Hello, world!');
+//console.error('This is an error message.');
+//console.warn('This is a warning message.');
+//console.info('This is an informational message.');
+//ver-Old, let, const
+//let age = 30;
+//age = 31;
+//console.log(age);
+//let score = 100;
+//score = 150;
+//console.log(score);
+
+// strings, number, boolean, null, undefined
+//string
+//premitive data types
+//const name = 'John';
+//number
+//const age = 25;
+//float
+//const rating = 4.5;
+//boolean
+//const isCool = true;
+//null
+//const x = null;
+//undefined
+//const y = undefined;
+//
+//let z;
+//console.log(typeof name);
+
+// strings, number, boolean, null, undefined
+
+//concatenation
+const name = 'John';
+const age = 25;
+console.log('My name is ' + name + ' and I am ' + age);
+
+//template strings
+console.log(`My name is ${name} and I am ${age}`);
+//string methods
+const s= 'Hello World';
