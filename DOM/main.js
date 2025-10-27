15c3258b3cd2b2f909368e33122a64170eaf9c35
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-10-27 00:13:46 -0400

    dom_css_html

---
+++ DOM/main.js
@@ -0,0 +1,14 @@
+//console.log(window)
+//alert("Hello, World!");
+
+
+// single element selector
+//const form = document.getElementById("my-form");
+// console.log(form);
+
+console.log(document.getElementById("my-form"));
+console.log(document.querySelector(".container"));
+
+
+
+// multiple element selector
