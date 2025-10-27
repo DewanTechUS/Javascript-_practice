420fb1a62a1146e98797b98a144f708bdbb8dce3
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-10-26 23:35:57 -0400

    dom

--- tm_learn/Constructor_Functions_&_Prototypes.js
+++ tm_learn/Constructor_Functions_&_Prototypes.js
@@ -26,12 +26,43 @@ function Person (firstname, lastname, dob) {
     this.getBirthYear = function() {
         return this.dob.getFullYear();
     }
+ this.getFullName = function() {
+        return `${this.firstname} ${this.lastname}`;
+}
 
+//Person.oprototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+
+}
+Person.prototype.getFullName = function() {
+    return `${this.firstname} ${this.lastname}`;
+}
+//}
+//class 
+
+class Person {
+    constructor(firstname, lastname, dob) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dob = new Date(dob);
+    }
+
+    getBirthYear() {
+        return this.dob.getFullYear();
+    }
+
+    getFullName() {
+        return `${this.firstname} ${this.lastname}`;
+    }
 }
 
 // instantiate object
 const person1 = new Person ('John', 'Doe', '1990-01-01');
 const person2 = new Person ('Dewan', 'Mahmud', '1992-02-02');
 console.log(person2);
+console.log(person1);
+// log the objects
 console.log(person1.getBirthYear());
 console.log(person2.getBirthYear());
+console.log(person1.getFullName());
+console.log(person2.getFullName());
